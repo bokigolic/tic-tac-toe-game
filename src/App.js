5777a98647afbdeb23184e8f3d9e053a1a26b360
bokigolic <bokigolic32@gmail.com>
2022-11-10 23:54:57 -0500

    board example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
-import './App.css';
+import './App.scss';
+import TicTacToe from './components/TicTacToe';
 
 function App() {
   return (
     <div className="App">
-   <div>Tic Tac Toe</div>
+      <div className='bg-frame'>
+        <TicTacToe />
+      </div>
     </div>
   );
 }
---
+++ src/components/TicTacToe.jsx
@@ -0,0 +1,21 @@
+const TicTacToe = () => {
+  return (
+    <div>
+      <h1>Tic Tac Toe</h1>
+      <div className="board">
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+    
+
+      </div>
+    </div>
+  )
+};
+export default TicTacToe;
